735be637dd4cb6543f54c067c49db29b57d1f430
hacktar <info@inzhoop.com>
2017-06-01 10:22:31 +0200

    ENS support via Leth API

--- ENS/dapp_14/index.js
+++ ENS/dapp_14/index.js
@@ -4,63 +4,45 @@ var dappleth = (function(){
 	var btnLeft;
 	var btnRight;
 	var btnCenter;
-	var ensContract;
-	var ens;
-	var ethRegistrar;
-	var auctionRegistrarContract;
-	var deedContract;
-	var testRegistrar;
-	var fifsRegistrarContract;
-	var resolverContract;
-	var publicResolver;
-	var reverseRegistrarContract;
-	var reverseRegistrar;
 
 	 function init(id,ABI,Address){
 		console.log("init " + id);
 		GUID=id;    
-		dappContract=Address;    
+		dappContract=apiENS.address;    
 
 		btnRight = angular.element(document.querySelector('#rightButton'));
 		btnLeft = angular.element(document.querySelector('#leftButton'));
 		btnCenter = angular.element(document.querySelector('#centerButton'));
-
-		ensContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"resolver","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"label","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setSubnodeOwner","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"ttl","type":"uint64"}],"name":"setTTL","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"ttl","outputs":[{"name":"","type":"uint64"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"resolver","type":"address"}],"name":"setResolver","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"owner","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"owner","type":"address"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":true,"name":"label","type":"bytes32"},{"indexed":false,"name":"owner","type":"address"}],"name":"NewOwner","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"resolver","type":"address"}],"name":"NewResolver","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"node","type":"bytes32"},{"indexed":false,"name":"ttl","type":"uint64"}],"name":"NewTTL","type":"event"}]);
-		auctionRegistrarContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"releaseDeed","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"getAllowedTime","outputs":[{"name":"timestamp","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"unhashedName","type":"string"}],"name":"invalidateName","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"},{"name":"owner","type":"address"},{"name":"value","type":"uint256"},{"name":"salt","type":"bytes32"}],"name":"shaBid","outputs":[{"name":"sealedBid","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"bidder","type":"address"},{"name":"seal","type":"bytes32"}],"name":"cancelBid","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"entries","outputs":[{"name":"","type":"uint8"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ens","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"},{"name":"_value","type":"uint256"},{"name":"_salt","type":"bytes32"}],"name":"unsealBid","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"transferRegistrars","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"bytes32"}],"name":"sealedBids","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"state","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"},{"name":"newOwner","type":"address"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"},{"name":"_timestamp","type":"uint256"}],"name":"isAllowed","outputs":[{"name":"allowed","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"finalizeAuction","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"registryStarted","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"sealedBid","type":"bytes32"}],"name":"newBid","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"labels","type":"bytes32[]"}],"name":"eraseNode","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hashes","type":"bytes32[]"}],"name":"startAuctions","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"},{"name":"deed","type":"address"},{"name":"registrationDate","type":"uint256"}],"name":"acceptRegistrarTransfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"startAuction","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"rootNode","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hashes","type":"bytes32[]"},{"name":"sealedBid","type":"bytes32"}],"name":"startAuctionsAndBid","outputs":[],"payable":true,"type":"function"},{"inputs":[{"name":"_ens","type":"address"},{"name":"_rootNode","type":"bytes32"},{"name":"_startDate","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":false,"name":"registrationDate","type":"uint256"}],"name":"AuctionStarted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"bidder","type":"address"},{"indexed":false,"name":"deposit","type":"uint256"}],"name":"NewBid","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"status","type":"uint8"}],"name":"BidRevealed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"registrationDate","type":"uint256"}],"name":"HashRegistered","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":false,"name":"value","type":"uint256"}],"name":"HashReleased","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"name","type":"string"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"registrationDate","type":"uint256"}],"name":"HashInvalidated","type":"event"}]);
-		deedContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"creationDate","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"destroyDeed","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"registrar","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"value","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"previousOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newValue","type":"uint256"},{"name":"throwOnFailure","type":"bool"}],"name":"setBalance","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"refundRatio","type":"uint256"}],"name":"closeDeed","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newRegistrar","type":"address"}],"name":"setRegistrar","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"_owner","type":"address"}],"payable":true,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newOwner","type":"address"}],"name":"OwnerChanged","type":"event"},{"anonymous":false,"inputs":[],"name":"DeedClosed","type":"event"}]);
-		fifsRegistrarContract = web3.eth.contract([{"constant":true,"inputs":[],"name":"ens","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"expiryTimes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"subnode","type":"bytes32"},{"name":"owner","type":"address"}],"name":"register","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"rootNode","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"inputs":[{"name":"ensAddr","type":"address"},{"name":"node","type":"bytes32"}],"type":"constructor"}]);
-		resolverContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"interfaceID","type":"bytes4"}],"name":"supportsInterface","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"addr","outputs":[{"name":"ret","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"},{"name":"kind","type":"bytes32"}],"name":"has","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"addr","type":"address"}],"name":"setAddr","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"node","type":"bytes32"}],"name":"content","outputs":[{"name":"ret","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"node","type":"bytes32"},{"name":"hash","type":"bytes32"}],"name":"setContent","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"ensAddr","type":"address"}],"type":"constructor"},{"payable":false,"type":"fallback"}]);
-		reverseRegistrarContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"owner","type":"address"}],"name":"claim","outputs":[{"name":"node","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ens","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"}],"name":"node","outputs":[{"name":"ret","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"rootNode","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"inputs":[{"name":"ensAddr","type":"address"},{"name":"node","type":"bytes32"}],"payable":false,"type":"constructor"}]);
 
 	}
 
 	function setup(){
 		console.log("setup");
 
-		btnLeft.html(' Register');
-		btnLeft.attr('class','button button-smal button-icon icon ion-play');
-		btnLeft.attr('onclick','dappleth.testRegistrar()');
+		if(apiENS.suffix=="test"){
+			btnLeft.html(' Register');
+			btnLeft.attr('class','button button-smal button-icon icon ion-ios-compose');
+			btnLeft.attr('onclick','dappleth.testRegistrar()');			
+		}
 
 		btnCenter.html(' Owner');
-		btnCenter.attr('class','button button-smal button-icon icon ion-play');
+		btnCenter.attr('class','button button-smal button-icon icon ion-ios-refresh');
 		btnCenter.attr('onclick','dappleth.getOwner()');
 
 		btnRight.html(' Address');
-		btnRight.attr('class','button button-smal button-icon icon ion-play');
+		btnRight.attr('class','button button-smal button-icon icon ion-ios-refresh');
 		btnRight.attr('onclick','dappleth.getAddr()');
 
-		ens = ensContract.at(dappContract); //'0x112234455c3a32fd11230c42e7bccd4a84e02010'
-		publicResolver = resolverContract.at('0x4c641fb9bad9b60ef180c31f56051ce826d21a9a');
-		testRegistrar = fifsRegistrarContract.at(ens.owner(namehash('test')));
-		reverseRegistrar = reverseRegistrarContract.at(ens.owner(namehash('addr.reverse')));
-	  	
 	  	angular.element(document.querySelector('#address')).html(apiApp.account()) ;
+	  	angular.element(document.querySelector('#suffix')).html(apiENS.suffix) ;
+
+	  	sendMessage(dappContract,"hi there &#x1F44B; enjoy the ENS!");
 
 	}
 
 	function update(){
 		console.log("update");
-		apiUI.loadFade("loading...",500);
+		apiUI.loadFade("loading...",100);
 	}
 
 	function destroy(){
@@ -77,7 +59,7 @@ var dappleth = (function(){
 	function sendError(fromAddr, msg){
 		var mres = {
 			from: fromAddr,
-		    text: msg + "!",
+		    text: " &#x1F6D1; " + msg,
 		    date: new Date()
 		};
 
@@ -96,92 +78,34 @@ var dappleth = (function(){
 	  	update();
 	}
 
-	function namehash(name) {
-	    var node = '0x0000000000000000000000000000000000000000000000000000000000000000';
-	    if(name != '') {
-	        var labels = name.split(".");
-	        for(var i = labels.length - 1; i >= 0; i--) {
-	            node = web3.sha3(node + web3.sha3(labels[i]).slice(2), {encoding: 'hex'});
-	        }
-	    }
-	    return node.toString();
-	}
-
 	function testRegistrar() {
 	  	var name = angular.element(document.querySelector('#domain'))[0].value ;
-
-		apiUI.popupConfirm("Register name " + name + '.test',"Are you sure you want to register this name?").then(function(res){
-			//var expire = new Date(testRegistrar.expiryTimes(web3.sha3(name)).toNumber() * 1000)
-
-			testRegistrar.register(web3.sha3(name), apiApp.account(), 
-				{from: apiApp.account(), gas: 180000, gasPrice: web3.eth.gasPrice}, 
-				function(err,res){
-					if(err){
-						sendError(ens.address, err.message);					
-					}else{
-						sendMessage(ens.address, res);
-						setResolver(name + ".test")
-					}
-
-				}
-			);
+	  	var domain = name +  "." + apiENS.suffix;
+		apiUI.popupConfirm("Register name","Are you sure you want to register <br/><b>" + domain +"</b>?").then(function(res){
+			apiENS.registrarTest(name, apiApp.account()).then(function(res){
+				apiENS.setResolver(domain).then(function(res){
+					apiENS.setAddress(domain,apiApp.account()).then(function(res){
+						sendMessage(apiENS.address, "Registration " + domain +  " in progress... &#x2705;");
+					}, function(err){console.log(err)});
+				}, function(err){console.log(err)});
+			}, function(err){console.log(err)});					
 		});
 	}
 
-	function setResolver(name){
-	  	var addr = apiApp.account() ; //limited only to your wallet addr
-
-		ens.setResolver(namehash(name), publicResolver.address, 
-			{from: apiApp.account(), gas: 180000, gasPrice: web3.eth.gasPrice}, 
-			function(err,res){
-				if(err){
-					sendError(ens.address, err.message);
-				}else{
-					setAddr(name,addr);
-					sendMessage(ens.address, "resolver setted! " + res);
-				}
-
-			}
-		);
-	}
-
-	function setAddr(name, addr){
-		publicResolver.setAddr(namehash(name), addr, 
-			{from: apiApp.account(), gas: 180000, gasPrice: web3.eth.gasPrice}, 
-			function(err,res){
-				if(err){
-					sendError(ens.address, err.message);
-				}else{
-					sendMessage(ens.address, "Addr setted! " + res);
-				}
-
-			}
-		);
-	}
-
 	function getAddr() {
-	  var name = angular.element(document.querySelector('#domain'))[0].value + ".test";
-
-	  var node = namehash(name)
-	  var resolverAddress = ens.resolver(node);
-	  if(resolverAddress == '0x0000000000000000000000000000000000000000') {
-	    sendMessage(ens.address, "OPS!  Not found :-(");
-      	return resolverAddress; 
-	  }
-  	  var result = resolverContract.at(resolverAddress).addr(node);
-
-      sendMessage(ens.address, "That's the address </br>" + "<b>" + result + "</b>");
-
-	  return result;
+	  var name = angular.element(document.querySelector('#domain'))[0].value + "." + apiENS.suffix;
+	  var result = apiENS.getAddress(name);
+	  if(result=="not found") sendError(apiENS.address, "Not found &#x1F644;" )
+	  else sendMessage(apiENS.address, "That's the address </br>" + "<b>" + result + "</b>");
 	}
 
 	function getOwner() {
-	  var name = angular.element(document.querySelector('#domain'))[0].value + ".test";
-	  var owner = ens.owner(namehash(name));
-	  if(owner == "0x0000000000000000000000000000000000000000")
-	      sendMessage(ens.address, " Not found :-(");
+	  var name = angular.element(document.querySelector('#domain'))[0].value + "." + apiENS.suffix;
+	  var owner = apiENS.getOwner(name);
+	  if(owner == "Not found")
+	      sendError(apiENS.address, "Not found &#x1F644;");
 	  else
-      	  sendMessage(owner, name + " it's mine!");
+      	  sendMessage(owner, name + " is mine &#x2705;");
 
 	}
 
